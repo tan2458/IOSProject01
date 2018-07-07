aadbbcfc45f1db83c5a0ab0b739a4773fbed347a
tanjinyong <2458621203@qq.com>
2018-07-07 18:25:08 +0800

    学习“仿白俄客户端demo"  day01

---
+++ Project01/Project01/Classes/Main/Delegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhiteRussian
+//
+//  Created by George.tan on 2018/7/6.
+//  Copyright © 2018年 George.tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Project01/Project01/Classes/Main/Delegate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhiteRussian
+//
+//  Created by George.tan on 2018/7/6.
+//  Copyright © 2018年 George.tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
