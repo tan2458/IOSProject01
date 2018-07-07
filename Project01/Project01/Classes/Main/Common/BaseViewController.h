4eca6389ce8d6892baef2708f9b4a4c535c8ce3e
tanjinyong <2458621203@qq.com>
2018-07-07 18:41:24 +0800

    增加登录控制器

---
+++ Project01/Project01/Classes/Main/Common/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  Project01
+//
+//  Created by George.tan on 2018/7/7.
+//  Copyright © 2018年 George.tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
---
+++ Project01/Project01/Classes/Main/Login/BELoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  BELoginViewController.h
+//  Project01
+//
+//  Created by George.tan on 2018/7/7.
+//  Copyright © 2018年 George.tan. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BELoginViewController : BaseViewController
+
+@end
